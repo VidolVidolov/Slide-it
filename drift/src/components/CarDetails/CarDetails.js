70dfda28bba0863e53b8928f5bb99eac55575275
Vidol Vidolov <vivividolov@gmail.com>
2021-04-03 22:05:01 +0300

    weather input updated

--- drift/src/components/CarDetails/CarDetails.js
+++ drift/src/components/CarDetails/CarDetails.js
@@ -5,15 +5,9 @@ const CarDetails = () => {
         <div className='page-wrapper'>
             <div className='pseudo-side-menu'>
                 <h1 className='page-heading'>Details</h1>
-                <p className='paragraph-under-heading'>
-                    for SEAT IBIZA
-                </p>
+                <p className='paragraph-under-heading'>for BMW E92</p>
             </div>
-            <div className='page-content'>
-                <div className='home-card-wrapper'>
-                   
-                </div>
-            </div>
+            <div className='page-content'></div>
         </div>
     );
 };
--- drift/src/components/Weather/Weather.js
+++ drift/src/components/Weather/Weather.js
@@ -19,7 +19,7 @@ const Weather = ({ location, changeLocation, showWeather }) => {
         }
         await changeLocation(buffer);
     };
-    
+
     return (
         <div className={`${!showWeather && 'hidden-weather'} weather-content`}>
             <form onSubmit={handleSubmit}>
