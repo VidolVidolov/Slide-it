6404bbe0793930ecbf9c9d59ed5df3dfa4be631d
Vidol Vidolov <vivividolov@gmail.com>
2021-04-08 19:20:52 +0300

    Update router.js

--- api/router.js
+++ api/router.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const homeController = require('./Controllers/homeController');
 const userController = require('./Controllers/userController');
 const carsController = require('./Controllers/carsController');
-
+router.get('/', (req, res) => {
+    res.send('Its working!');
+});
 router.use('/api/users', userController);
-router.use('/api/cars', carsController)
+router.use('/api/cars', carsController);
 router.use('/api', homeController);
 
 module.exports = router;
