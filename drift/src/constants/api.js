4a1f2d3c4865614104c49f005e4251222643cfd8
Vidol Vidolov <vivividolov@gmail.com>
2021-04-09 17:27:51 +0300

    Laptop compatible app now

--- drift/src/constants/api.js
+++ drift/src/constants/api.js
@@ -1,1 +1,6 @@
-export const BASE_URL = 'http://localhost:4000/api';
+const serverAddress = {
+    development: 'http://localhost:4000/api',
+    production: 'https://slide-it-v.herokuapp.com/api',
+};
+
+export const BASE_URL = serverAddress[process.env.NODE_ENV];
