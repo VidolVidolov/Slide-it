f3bc6a835f1a0125dcbb22dc50ec0abcbf4efcbd
Vidol Vidolov <vivividolov@gmail.com>
2022-02-09 11:11:11 +0200

    validations made with PropTypes library in homeCard

--- drift/src/shared/HomeCard/HomeCard.js
+++ drift/src/shared/HomeCard/HomeCard.js
@@ -1,23 +1,46 @@
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './HomeCard.scss';
 
-const HomeCard = ({ car }) => {
+
+const HomeCard = ({
+    car: {
+        brand,
+        model,
+        picture,
+        horsePower,
+        videoLink,
+        _id
+    } }) => {
     return (
         <div className='home-card'>
             <h2>
-                {car.brand}: {car.model}
+                {brand}: {model}
             </h2>
             <div className='image-wrapper'>
-                <img src={car.picture} alt={car.model} />
+                <img src={picture} alt={model} />
             </div>
             <div className='car-info-wrapper'>
-                <p>POWER: {car.horsePower} HP</p>
-                <p onClick={() => window.open(car.videoLink)} className='video-link-button'>
+                <p>POWER: {horsePower} HP</p>
+                <p onClick={() => window.open(videoLink)} className='video-link-button'>
                     click to see car's video
                 </p>
             </div>
-            <Link to={`/${car._id}/details`}>view more</Link>
+            <Link to={`/${_id}/details`}>view more</Link>
         </div>
     );
 };
+
+HomeCard.propTypes = {
+    car: PropTypes.shape({
+        brand: PropTypes.string,
+        model: PropTypes.string,
+        picture: PropTypes.string,
+        horsePower: PropTypes.number,
+        videoLink: PropTypes.string,
+        _id: PropTypes.string,
+
+    }),
+};
+
 export default HomeCard;
